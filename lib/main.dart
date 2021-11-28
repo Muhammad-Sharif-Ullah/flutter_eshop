8308ed56f4b7bf7c74928c7baf3e5323689543ba
unknown <sharifnkbd@gmail.com>
2021-11-29 02:12:45 +0600

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eshop/routes/app_pages.dart';
 import 'package:flutter_eshop/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const FlutterEshop());
 }
 
